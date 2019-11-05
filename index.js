c59ca66b5a45946577db3f894fdebc9cdd9e0fc2
Charles Camisa <charles@kalekoko.com>
2019-11-05 23:37:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
+var katzDeliLine = []; // Declcares an empty array called katzDeliLine
+
+function takeANumber(katzDeli, name) { //I'm creating a function called takeANumber with katzDeli and name as their value
+   katzDeli.push(name) //Pushes element to the end of the array
+   return `Welcome, ${name}. You are number ${katzDeli.length} /* When I call the function it will return the name and counts the array number in katzDeliLine */ in line.`;
+}
+   
+   function nowServing(katzDeli) {
+  if (katzDeli.length > 0) { //In this case 2
+    var nowServing = katzDeli[0]; // Grabs the first index/name Ada
+    katzDeli.shift(); // Removes Ada from the katzDelinLine array
+    return `Currently serving ${nowServing}.`; // Returns "Currently serving Ada."
+  } else { // If katzDeliLine array is empty it returns
+    return `There is nobody waiting to be served!`;
+  }
+// nowserving(katzDeliLine) returns "Currently serving Ada."
+// katzDelinLine is now "Steve" because this function shifts out "Ada"
+   
 function currentLine(line) {
-  if (!line.length) {
-    return "The line is currently empty."
+  if (line.length === 0) {
+    return `The line is currently empty.`;
+  } else {
+
+    for (var i = 0; i < line.length; i++) {
+      array.push(` ${i+1}. ${line[i]}`);
+    }
+    return `The line is currently:` + array;
   }
-
-  const numbersAndNames = []
-
-  for (let i = 0, l = line.length; i < l; i++) {
-    numbersAndNames.push(`${i + 1}. ${line[i]}`)
-  }
-
-  /* Keep in mind, join() is a a shorthand for looping and building
-   * into a String. */
-  return `The line is currently: ${numbersAndNames.join(', ')}`
-};
-
-function nowServing(line) {
-  if (!line.length) {
-    return "There is nobody waiting to be served!"
-  }
-
-  return `Currently serving ${line.shift()}.`
-};
-
-function takeANumber(line, name) {
-  line.push(name)
-
-  return `Welcome, ${name}. You are number ${line.length} in line.`
-};
+}
+// Calling the currentLine(katzDeliLine) function checks the line lengh index in the katzDeliLine array
